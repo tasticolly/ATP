6e8182060c1d8d01d67822bff8d36a9f9c05ec4e
Krekhov Nikolai <krekhov.na@phystech.edu>
2022-03-02 01:12:12 +0300

    ohhhh

--- task1/run.sh
+++ task1/run.sh
@@ -15,9 +15,8 @@ esac
 shift
 done
 ORIGIN_DIR=$(pwd)
+mkdir $BACKUP_FOLDER
 cd $BACKUP_FOLDER
-mkdir backup_folder
-cd backup_folder
 BACKUP_FOLDER=$(pwd)
 cd $ORIGIN_DIR
 cd $INPUT_FOLDER
@@ -28,3 +27,8 @@ cp $x $BACKUP_FOLDER/$x
 done
 tar -czf $BACKUP_FOLDER/../$BACKUP_ARCHIVE_NAME $BACKUP_FOLDER 2>"/dev/null"
 echo "done"
+
+#mkdir $BACKUP_FOLDER
+#find  $INPUT_FOLDER -name "*.$EXTENSION" | xargs cp --parents -t $BACKUP_FOLDER
+#tar -czf $BACKUP_ARCHIVE_NAME $BACKUP_FOLDER
+
