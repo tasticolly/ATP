487f7d27b3aa475bc2fbf0102fa9eb33230525d6
Krekhov Nikolai <krekhov.na@phystech.edu>
2022-03-01 23:41:10 +0300

    Add task1

---
+++ task1/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while [ -n "$1" ]
+do
+case "$1" in
+"--input_folder") INPUT_FOLDER="$2" 
+shift;;
+"--extension") EXTENSION="$2"
+shift;;
+"--backup_folder") BACKUP_FOLDER="$2"
+shift;;
+"--backup_archive_name") BACKUP_ARCHIVE_NAME="$2"
+shift;;
+esac
+shift
+done
+mkdir $BACKUP_FOLDER/backup_folder
+ORIGIN_DIR=$(pwd)
+cd $BACKUP_FOLDER/backup_folder
+BACKUP_FOLDER=$(pwd)
+cd $ORIGIN_DIR/$INPUT_FOLDER
+for x in $(find . -name "*.$EXTENSION") #| xargs mkdir -p && xargs cp $BACKUP_FOLDER/backup_folder
+do
+mkdir -p $BACKUP_FOLDER/$(dirname $x)
+cp $x $BACKUP_FOLDER/$x
+done
+tar -czf $BACKUP_FOLDER/../$BACKUP_ARCHIVE_NAME $BACKUP_FOLDER 2>"/dev/null"
+echo "done"
+
+
+
+
